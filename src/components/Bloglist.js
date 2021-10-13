afefaa7dba0015e7418df93fb4023b907930ced8
farazkhanfk7 <farazkhan138@gmail.com>
2021-10-13 13:34:36 +0530

    props

---
+++ src/components/Bloglist.js
@@ -0,0 +1,21 @@
+const Bloglist = ({blogs,title}) => {
+
+    // Or we use (props) => {} and then, but here is used destructuring
+    // const blogs = props.blogs
+    // const title = props.title
+
+    return ( 
+        <div className="blog-list">
+            <h2>{title}</h2>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>by {blog.author}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+ 
+export default Bloglist
+;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,18 @@
 import { useState } from 'react';
+import Bloglist from './Bloglist';
 
 const Home = () => {
 
     const [blogs, setblogs] = useState([
         { title : "New Website", body:"LOrem ipsum..", author:"faraz", id:1},
         { title : "Sports", body:"manchester vs..", author:"hasan", id:2},
-        { title : "Cars ", body:"LOrem ipsum..", author:"batcypher", id:3},
+        { title : "Cars ", body:"LOrem ipsum..", author:"faraz", id:3},
     ])
 
     return (
         <div className="home">
-            {blogs.map((blog)=>(
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>by {blog.author}</p>
-                </div>
-            ))}
+            <Bloglist blogs={blogs} title="All Blogs" />
+            <Bloglist blogs={blogs.filter((blogs)=>blogs.author==="faraz")} title="Faraz's Blogs" />
         </div>
         );
 }
