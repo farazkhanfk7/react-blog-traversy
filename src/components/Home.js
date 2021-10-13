b32f5bfb6fc18827aa929e578dac1150e5f7acd1
farazkhanfk7 <farazkhan138@gmail.com>
2021-10-13 09:15:13 +0530

    08 useState hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+
 const Home = () => {
-    return ( 
-        <h1>Homepage</h1>
-     );
+
+    const [name, setname] = useState('Faraz')
+
+    const handleClick = () => {
+        setname('Hasan')
+    }
+
+    return (
+        <div className="home">
+            <h1>Homepage</h1>
+            <h4>{ name }</h4>
+            <button onClick={handleClick}>Change Name</button>
+            {/* Incase you want to pass parameter to event handler function
+            <button onClick={()=>handleClick('faraz')}>Click</button> */}
+        </div>
+        );
 }
  
 export default Home;
