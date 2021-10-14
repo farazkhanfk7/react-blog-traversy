52942677a30f93e28eeb30034b1a8f0dca892935
farazkhanfk7 <farazkhan138@gmail.com>
2021-10-14 15:16:43 +0530

    error handling in fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,16 +5,24 @@ const Home = () => {
 
     const [blogs, setblogs] = useState(null)
     const [pending, setpending] = useState(true)
+    const [error, seterror] = useState(false)
  
     useEffect(()=>{
         // using settimeout here just to demonstrate the time server takes to fetch
         setTimeout(() => {
             fetch("http://localhost:8000/blogs")
             .then((res) => {
+                if (!res.ok){
+                    throw Error("Couldn't get data for this resource.")
+                }
                 return res.json()
             }).then(data => {
                 setblogs(data)
                 setpending(false)
+            }).catch(err => {
+                setpending(false)
+                seterror(err.message)
+                console.log(err.message)
             })
         }, 1000);
     })
@@ -26,6 +34,7 @@ const Home = () => {
  
     return (
         <div className="home">
+            { error && <div>{ error }</div> }
             { pending && <div>Loading...</div> }
             {blogs && <Bloglist blogs={blogs} title="All Blogs"/>}
         </div>
