79d80e1f4ac316b6a19c928bb216acde169d1396
farazkhanfk7 <farazkhan138@gmail.com>
2021-10-14 20:29:03 +0530

    router and after that abortcontroller

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Bat Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
                 <a href="/login" style={{
                     color : "white",
                     backgroundColor : "#f1356d",
--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -1,14 +1,18 @@
 import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
+
     const [data, setdata] = useState(null)
     const [pending, setpending] = useState(true)
     const [error, seterror] = useState(false)
 
     useEffect(()=>{
         // using settimeout here just to demonstrate the time server takes to fetch
+
+        const abortcont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url , {signal : abortcont.signal })
             .then((res) => {
                 if (!res.ok){
                     throw Error("Couldn't get data for this resource.")
@@ -18,12 +22,18 @@ const useFetch = (url) => {
                 setdata(data)
                 setpending(false)
             }).catch(err => {
-                setpending(false)
-                seterror(err.message)
-                console.log(err.message)
+                if (err.name === 'AbortError'){
+                    console.log("fetch aborted")
+                } else {
+                    setpending(false)
+                    seterror(err.message)
+                    console.log(err.message)
+                }    
             })
         }, 1000);
-    });
+
+        return () => abortcont.abort();
+    }, [url]);
 
     return {data,pending,error}
 }
