a3fbcdb85b33bc8b31f172186b42c4300447f554
farazkhanfk7 <farazkhan138@gmail.com>
2021-10-15 11:32:08 +0530

    not found pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home'
 import Create from './components/Create'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import BlogDetail from './components/BlogDetail';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           </Route>
           <Route path="/blogs/:id">
             <BlogDetail/>
+          </Route>
+          <Route path="*">
+            <NotFound/>
           </Route>
         </Switch>
       </div>
